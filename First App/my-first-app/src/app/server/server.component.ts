07e35a252395b5eca94e9b221ae186d605f4c862
IshmaelFifth <ishmaelillo1@users.noreply.github.com>
2017-11-07 16:29:14 -0600

    EndOfComponents

--- First App/my-first-app/src/app/server/server.component.ts
+++ First App/my-first-app/src/app/server/server.component.ts
@@ -4,5 +4,9 @@ import {Component} from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-   
+   serverID:number =10;
+   serverStatus='offline';
+   getServerStatus(){
+       return this.serverStatus;
+   }
 }
